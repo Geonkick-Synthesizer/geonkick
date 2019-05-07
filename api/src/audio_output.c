6dd9c4f1e4ea99de221ba8d81a0c5bac10bc9877
Iurie Nistor <nistor@iurie.org>
2019-05-07 11:19:44 +0100

    fix swaping buffers

--- api/src/audio_output.c
+++ api/src/audio_output.c
@@ -187,6 +187,7 @@ gkick_audio_output_set_limiter_callback(struct gkick_audio_output *audio_output,
 
 void gkick_audio_swap_buffers(struct gkick_audio_output *audio_output)
 {
+        gkick_buffer_reset((struct gkick_buffer*)audio_output->playing_buffer);
         char *buff = audio_output->updated_buffer;
         audio_output->updated_buffer = audio_output->playing_buffer;
         audio_output->playing_buffer = buff;
