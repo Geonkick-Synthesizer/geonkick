35b924839b66eec3b628318d1c23bb0a40e6cfa0
Iurie Nistor <iur.nistor@gmail.com>
2022-01-30 18:57:56 +0200

    remove/disable some unused stuff from VST3 SDK

--- 3rdparty/vst3_sdk/vstgui4/vstgui/uidescription/editing/createuidescdata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo `pwd`
-mkdir -p "$1"
-rm -f "$1/editoruidesc.h"
-echo -n "constexpr auto editorUIDesc = R\"====(" >> "$1/editoruidesc.h"
-cat uidescriptioneditor.uidesc >> "$1/editoruidesc.h"
-echo ")====\";" >> "$1/editoruidesc.h"
-
