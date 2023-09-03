a7071f073e6c621bd2332117ee5d8631390e01ad
Iurie Nistor <iuriehn@gmail.com>
2023-09-03 20:15:14 +0300

    cleaup some links and emails

--- dsp/src/oscillator.h
+++ dsp/src/oscillator.h
@@ -2,7 +2,7 @@
  * File name: oscillator.c
  * Project: Geonkick (A kick synthesizer)
  *
- * Copyright (C) 2017 Iurie Nistor (http://iuriepage.wordpress.com)
+ * Copyright (C) 2017 Iurie Nistor
  *
  * This file is part of Geonkick.
  *
