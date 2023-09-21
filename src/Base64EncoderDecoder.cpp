b03365612728ea055faa13f3f814362e87ffc270
Iurie Nistor <iuriehn@gmail.com>
2023-09-21 20:10:14 +0300

    error check for encode/decode base64

--- src/Base64EncoderDecoder.cpp
+++ src/Base64EncoderDecoder.cpp
@@ -31,42 +31,64 @@ F *
 
 std::string Base64EncoderDecoder::encode(const std::vector<float> &data)
 {
-  std::vector<unsigned char> bytes(data.size() * sizeof(float));
-  std::memcpy(bytes.data(), data.data(), bytes.size());
+        auto b64 = BIO_new(BIO_f_base64());
+        if (!b64) {
+                GEONKICK_LOG_ERROR("can't create bio64");
+                return std::string();
+        }
 
-  BIO *bio, *b64;
-  BUF_MEM *bufferPtr;
+        BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+        auto bio = BIO_new(BIO_s_mem());
+        if (!bio) {
+                GEONKICK_LOG_ERROR("can't create bio");
+                BIO_free_all(b64);
+                return {};
+        }
 
-  b64 = BIO_new(BIO_f_base64());
-  BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-  bio = BIO_new(BIO_s_mem());
-  BIO_push(b64, bio);
+        BIO_push(b64, bio);
+        BIO_write(b64, data.data(), static_cast<int>(data.size() * sizeof(float)));
+        BIO_flush(b64);
+        BUF_MEM *bufferPtr;
+        if (BIO_get_mem_ptr(b64, &bufferPtr) != 1) {
+               GEONKICK_LOG_ERROR("can't read BIO ptr");
+               BIO_free_all(b64);
+               return {};
+        }
 
-  BIO_write(b64, bytes.data(), static_cast<int>(bytes.size()));
-  BIO_flush(b64);
-  BIO_get_mem_ptr(b64, &bufferPtr);
-
-  std::string encoded(bufferPtr->data, bufferPtr->length);
-
-  BIO_free_all(b64);
-  return encoded;
+        std::string encoded(bufferPtr->data, bufferPtr->length);
+        BIO_free_all(b64);
+        GEONKICK_LOG_DEBUG("ecoded: " << encoded);
+        return encoded;
 }
 
 std::vector<float> Base64EncoderDecoder::decode(const std::string &input)
 {
-  BIO *bio, *b64;
+        auto bio = BIO_new_mem_buf(input.c_str(), -1);
+        if (!bio) {
+                GEONKICK_LOG_ERROR("can't create bio");
+                return {};
+        }
+        auto b64 = BIO_new(BIO_f_base64());
+        if (!b64) {
+                GEONKICK_LOG_ERROR("can't create bio64");
+                BIO_free_all(bio);
+                return {};
+        }
+        BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+        BIO_push(b64, bio);
 
-  bio = BIO_new_mem_buf(input.c_str(), -1);
-  b64 = BIO_new(BIO_f_base64());
-  BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-  BIO_push(b64, bio);
+        std::string outputBuffer(input.length(), 0x0);
+        auto decodedLength = BIO_read(b64, outputBuffer.data(), input.length());
+        if (decodedLength < 1 ) {
+                GEONKICK_LOG_ERROR("can't decode base64");
+        } else {
+                GEONKICK_LOG_DEBUG("decoded lengh: " << decodedLength);
+                std::vector<float> decoded(static_cast<size_t>(decodedLength / sizeof(float)));
+                std::memcpy(decoded.data(), outputBuffer.data(), decodedLength);
+                BIO_free_all(b64);
+                return decoded;
+        }
 
-  char outputBuffer[input.length()];
-  int decodedLength = BIO_read(b64, outputBuffer, input.length());
-
-  std::vector<float> decoded(decodedLength / sizeof(float));
-  std::memcpy(decoded.data(), outputBuffer, decodedLength);
-
-  BIO_free_all(b64);
-  return decoded;
+        BIO_free_all(b64);
+        return {};
 }
